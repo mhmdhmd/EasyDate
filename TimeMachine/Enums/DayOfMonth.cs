8b3b99662b7b611b156a96a202d579cb760f1d95
hamid.mohammadi <hamid.mohammadi@meditechsys.com>
2024-06-07 15:52:54 +0330

    Added enumerations

---
+++ TimeMachine/Enums/DayOfMonth.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeMachine.Enums
+{
+    public enum DayOfMonth
+    {
+        First = 1,
+        Second,
+        Third,
+        Fourth,
+        Fifth,
+        Sixth,
+        Seventh,
+        Eighth,
+        Ninth,
+        Tenth,
+        Eleventh,
+        Twelfth,
+        Thirteenth,
+        Fourteenth,
+        Fifteenth,
+        Sixteenth,
+        Seventeenth,
+        Eighteenth,
+        Nineteenth,
+        Twentieth,
+        TwentyFirst,
+        TwentySecond,
+        TwentyThird,
+        TwentyFourth,
+        TwentyFifth,
+        TwentySixth,
+        TwentySeventh,
+        TwentyEighth,
+        TwentyNinth,
+        Thirtieth,
+        ThirtyFirst,
+        Last
+    }
+}
---
+++ TimeMachine/Enums/Months.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeMachine.Enums
+{
+    public enum Months
+    {
+        Jan = 1,
+        Feb,
+        Mar,
+        Apr,
+        May,
+        Jun,
+        Jul,
+        Aug,
+        Sep,
+        Oct,
+        Nov,
+        Dec
+    }
+}
