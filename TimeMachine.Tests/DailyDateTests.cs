d57f791ae0f7c2340bd80dcd8055da44c074b318
mhmdhmd <hmd.tradeinfo@gmail.com>
2024-06-08 13:33:48 +0330

    Clean Code

--- TimeMachine.Tests/DailyDateTests.cs
+++ TimeMachine.Tests/DailyDateTests.cs
@@ -1,5 +1,4 @@
 using FluentAssertions;
-using TimeMachine;
 
 namespace TimeMachine.Tests
 {
--- TimeMachine.Tests/YearlyDateTests.cs
+++ TimeMachine.Tests/YearlyDateTests.cs
@@ -1,10 +1,4 @@
 ﻿using FluentAssertions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TimeMachine;
 
 namespace TimeMachine.Tests
 {
